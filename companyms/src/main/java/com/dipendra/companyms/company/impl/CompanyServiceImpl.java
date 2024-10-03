da60aa69089a91b541c5e2f56db66461a01913e4
Dipendra Jawariya <dipendrajawariya20@gmail.com>
2024-10-03 14:19:11 +0530

    Company Microservice Created

---
+++ companyms/src/main/java/com/dipendra/companyms/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,58 @@
+package com.dipendra.companyms.company.impl;
+
+import com.dipendra.companyms.company.Company;
+import com.dipendra.companyms.company.CompanyRepository;
+import com.dipendra.companyms.company.CompanyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company,Long id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if(companyOptional.isPresent()) {
+            Company company1 = companyOptional.get();
+
+            company1.setDescription(company.getDescription());
+//            company1.setJobs(company.getJobs());
+            company1.setName(company.getName());
+            companyRepository.save(company1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean deleteCompanyById(Long id) {
+        if(companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+}
