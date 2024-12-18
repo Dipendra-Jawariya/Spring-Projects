22b48a643526d98ecff8956c62e60f119dfae7cb
Dipendra Jawariya <dipendrajawariya20@gmail.com>
2024-12-18 13:16:15 +0530

    API gateway Added LOAD BALANCING

--- jobms/src/main/java/com/dipendra/jobms/JobmsApplication.java
+++ jobms/src/main/java/com/dipendra/jobms/JobmsApplication.java
@@ -2,6 +2,7 @@ package com.dipendra.jobms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
